68938311d490b15a92aeb6596a6b8dc76902c46d
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-06-13 23:46:39 +0800

    Change BG for different weather conditions

--- src/index.js
+++ src/index.js
@@ -1,86 +1,139 @@
-const GIPHY_API_KEY = "Yw4tONoc5wmCrKPVxoEUbFuCULiYewG0";
+const WeatherApp = function() {
+    const GIPHY_API_KEY = "Yw4tONoc5wmCrKPVxoEUbFuCULiYewG0";
+    const ERROR = "ERROR";
+    const DEFAULT_BG_GIPHY_ID = "ZxLr4sFdcSRVhajXli";
 
-function setBackgroundImage() {
-    const img = document.querySelector('img');
-    const bgGiphyId = "ZxLr4sFdcSRVhajXli";
-    const rainGiphyId = "3o7btWO4T2Wp97lbgc";
-    const normalGiphyId = "ZxLr4sFdcSRVhajXli";
-
-    fetch(`https://api.giphy.com/v1/gifs/${bgGiphyId}?api_key=${GIPHY_API_KEY}`, {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        document.body.style.backgroundImage = `url('${response.data.images.original.url}')`;
-    });
-}
-
-function getWeatherByLocation(location) {
-    const WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=99f5272715d7e5ecca0f597fb1ad1d54`;
-    
-    let weatherData = fetch(WEATHER_URL)
-    .then(function(response) {
-        // console.log(response.json());
-        return response.json();
-    })
-    .then(function(response) {
-        //WEATHER CONDITIONS
-        // Group 2xx: Thunderstorm
-        // Group 3xx: Drizzle
-        // Group 5xx: Rain
-        // Group 6xx: Snow
-        // Group 7xx: Atmosphere
-        // Group 800: Clear
-        // Group 80x: Clouds
-        // visit https://openweathermap.org/weather-conditions
-        //celsius = metric
-        // fahreinheight = imperial
-        // icon http://openweathermap.org/img/wn/10d@2x.png
-
-        let temp = response['main']['temp'];
-        let city = response['name'];
-        let weather = response['weather'][0];
-        let country = response['sys']['country'];
-        let weatherDescription = weather['description'];
-        let weatherIcon = weather['icon'];
-        console.log({
-            temperature : temp,
-            city: city,
-            weatherDescription : weatherDescription,
-            weatherIcon: weatherIcon,
-            country: country
+    function setBackgroundImage(bgGiphyId) {
+        const img = document.querySelector('img');
+        fetch(`https://api.giphy.com/v1/gifs/${bgGiphyId}?api_key=${GIPHY_API_KEY}`, 
+               {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            document.body.style.backgroundImage = `url('${response.data.images.original.url}')`;
         });
-        return {
-            temperature : temp,
-            city: city,
-            weatherDescription : weatherDescription,
-            weatherIcon: weatherIcon,
-            country: country
-        };
+    }
+    
+    function getWeatherByLocation(location) {
+        const WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=99f5272715d7e5ecca0f597fb1ad1d54`;
         
-    });
+        let weatherData = fetch(WEATHER_URL)
+        .then(function(response) {
+            if(!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(function(response) {
+            let temp = response['main']['temp'];
+            let city = response['name'];
+            let weather = response['weather'][0];
+            let country = response['sys']['country'];
+            let weatherDescription = weather['description'];
+            let weatherIcon = weather['icon'];
+            
+            if(country == undefined) {
+                country = 'N/A';
+            }
+    
+            return {
+                id: response['weather'][0]['id'],
+                temperature : temp,
+                city: city,
+                weatherDescription : weatherDescription,
+                weatherIcon: weatherIcon,
+                country: country
+            };
+            
+        }).catch(function() {
+            return ERROR;
+        });
+    
+        return weatherData;
+    }
+    
+    function setWeatherResults(location) {
+        getWeatherByLocation(location)
+        .then(
+            function(results) {
+                changeDisplay(results);
+            }
+        );
+    }
 
-    return weatherData;
-}
+    function changeDisplay(results) {
+        let descriptionElement = document.getElementById('description');
+        let cityElement = document.getElementById('city');
+        let tempElement = document.getElementById('temperature');
+        let imgElement = document.getElementById('weather-icon');
 
-function setWeatherResults() {
-    getWeatherByLocation('London')
-    .then(
-        function(results) {
-            let descriptionElement = document.getElementById('description');
-            let cityElement = document.getElementById('city');
-            let tempElement = document.getElementById('temperature');
-            let imgElement = document.getElementById('weather-icon');
-        
-            console.log(results);
+        if(results === ERROR) {
+            descriptionElement.innerText = "NOT FOUND";
+            imgElement.setAttribute('src', "notfound.png");
+            cityElement.innerText = '';
+            tempElement.innerHTML = '';
+            setBackgroundImage(DEFAULT_BG_GIPHY_ID);
+        } else {
             descriptionElement.innerText = results['weatherDescription'];
             imgElement.setAttribute('src', `http://openweathermap.org/img/wn/${results['weatherIcon']}@2x.png`)
             cityElement.innerText = `${results['city']}, ${results['country']}`;
-            tempElement.innerText = results['temperature'];
+            tempElement.innerHTML = `${results['temperature']}&deg;C`;
+            changeBgDependingOnDescription(results['id']);
         }
-    );
-}
+    }
+    
+    function addEventListenerToSearchLocationButton() {
+        let searchBtn = document.getElementById('search-location');
+        let input = document.getElementById("input-location");
+    
+        searchBtn.addEventListener("click", function() {setWeatherResults(input.value)});
+        document.addEventListener("keypress", function(e) {
+            if(e.key === 'Enter') {
+                if(input.value !== '') {
+                    setWeatherResults(input.value);
+                    input.value = '';
+                }
+                
+            }
+        });
+    }
+    
+    function changeBgDependingOnDescription(id) {
+        let giphyId = DEFAULT_BG_GIPHY_ID;
+        const THUNDERSTORM_BG_GIPGY_ID = "3osxYzIQRqN4DOEddC";
+        const DRIZZLE_BG_GIPGY_ID = "xT9GEz2CeU9uaI2KZi";
+        const RAIN_BG_GIPGY_ID = "3oKIPstwMF15FghbYQ";
+        const SNOW_BG_GIPGY_ID = "6YNgoTEPs6vZe";
+        const ATMOSPHERE_BG_GIPGY_ID = "hTgdcNl4LXLlOelNvZ";
+        const CLEAR_BG_GIPGY_ID = "wNipYAoZ3iaEE";
+        const CLOUDS_BG_GIPGY_ID = "oNXIP3xpr00k05NVPQ";
 
-setBackgroundImage();
-// getWeatherByLocation('Cebu');
-setWeatherResults();
+        if(id >= 200 && id < 300) {
+            giphyId = THUNDERSTORM_BG_GIPGY_ID;
+        } else if(id >= 300 && id < 400) {
+            giphyId = DRIZZLE_BG_GIPGY_ID;
+        } else if(id >= 500 && id < 600) {
+            giphyId = RAIN_BG_GIPGY_ID;
+        } else if(id >= 600 && id < 700) {
+            giphyId = SNOW_BG_GIPGY_ID;
+        } else if(id >= 700 && id < 800) {
+            giphyId = ATMOSPHERE_BG_GIPGY_ID;
+        } else if(id == 800) {
+            giphyId = CLEAR_BG_GIPGY_ID;
+        } else if(id >= 800) {
+            giphyId = CLOUDS_BG_GIPGY_ID;
+        } 
+        
+        setBackgroundImage(giphyId);
+    }
+    
+    function init() {
+        setWeatherResults('Cebu');
+        addEventListenerToSearchLocationButton();
+    }
+
+    return {init}
+}();
+
+WeatherApp.init();
